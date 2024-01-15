1743c81845e177cee0c43a11c9cc0cd7afb66ccf
Nguyen Trung <trunghy2k4111@gmail.com>
2024-01-15 20:47:56 +0700

    update code

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/AddToCardVueJs/',
+  base:'AddToCardVueJs',
   plugins: [
     vue(),
   ],
